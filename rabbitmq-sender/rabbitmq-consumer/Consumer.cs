9e94b66bf29c53ca1d7692fa8d34b0b6bb7901da
dimitar_georgiev <dimitar.georgiev99@abv.bg>
2022-02-09 12:11:14 +0200

    initial commit

---
+++ rabbitmq-sender/rabbitmq-consumer/Consumer.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Consumer
+{
+    class Consumer
+    {
+        static void Main(string[] args)
+        {
+            var connect = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connect.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                Console.WriteLine("Waiting for messages.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine("Received '{0}':'{1}'",
+                        routingKey,
+                        message);
+                };
+                channel.BasicConsume(
+                    queue: "message",
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ rabbitmq-sender/rabbitmq-sender/Sender.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.Client
+{
+    public class RabbitMQClient
+    {
+        static void Main(string[] args)
+        {
+            var connect = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = connect.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                var routingKey = "message";
+                var body = Encoding.UTF8.GetBytes("Hello");
+
+                while (true)
+                {
+                    channel.BasicPublish(
+                        exchange: "direct_logs",
+                        routingKey: routingKey,
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine("Sent '{0}': Hello", routingKey);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
